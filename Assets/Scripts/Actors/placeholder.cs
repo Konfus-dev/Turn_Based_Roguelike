e9b626d7fced8926727a1c2ee55ec130caf4c048
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2020-08-14 22:37:31 -0700

    Update to 2019.4.8f1

--- Assets/Scripts/Actors/placeholder.cs
+++ Assets/Scripts/Actors/placeholder.cs
@@ -1,18 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// placeholder
+   
 
-public class placeholder : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
