869ffb17ffcca8c162dbc880da0ac1d654e92a16
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2020-08-14 22:57:11 -0700

    Added interactables

---
+++ Assets/Scripts/Interactables/Chest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Interactable
+{
+
+    //private Animator animator;                    //Used to store a reference to the interactables's animator component.
+
+    void Awake()
+    {
+        //animator = GetComponent<Animator>();
+    }
+
+
+    public override void Interact<T>(T component)
+    {
+        // Player player = component as Player;
+
+        // OpenChest(player.Inventory);
+    }
+
+    private void OpenChest(/*Inventory playerInventory*/)
+    {
+        //Debug.Log("Opened Chest!");
+    }
+}
---
+++ Assets/Scripts/Interactables/Key.cs
@@ -0,0 +1,9 @@
+﻿
+public class Key : Interactable
+{
+    // Start is called before the first frame update
+    public override void Interact<T>(T component)
+    {
+        gameObject.SetActive(false);
+    }
+}
