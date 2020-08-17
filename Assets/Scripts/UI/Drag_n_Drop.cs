97a6daab740ea9da8989cce7b9999a3faaf5df84
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2020-08-16 19:49:35 -0700

    Added drag and drop system, inventory is not yet complete but is well on its way!

---
+++ Assets/Scripts/UI/Drag_n_Drop.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag_n_Drop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+
+    [SerializeField] private Canvas Canvas;
+    private RectTransform RectTransform;
+    private CanvasGroup CanvasGroup;
+    
+    void Awake()
+    {
+        this.RectTransform = this.GetComponent<RectTransform>();
+        this.CanvasGroup = this.GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        CanvasGroup.blocksRaycasts = false;
+        CanvasGroup.alpha = .5f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        this.RectTransform.anchoredPosition += eventData.delta / this.Canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        CanvasGroup.blocksRaycasts = true;
+        CanvasGroup.alpha = 1;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Dropped");
+    }
+}
+
---
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if(eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().sizeDelta = 
+                new Vector2(this.GetComponent<RectTransform>().sizeDelta.x * 2, this.GetComponent<RectTransform>().sizeDelta.y * 2);
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
