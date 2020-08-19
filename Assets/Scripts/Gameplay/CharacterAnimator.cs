a31952e6f0350515c4b60e43630c0240d1bef20e
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2020-08-19 12:00:52 -0700

    Added character animations w/ dotween

---
+++ Assets/Scripts/Gameplay/CharacterAnimator.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+
+    private Transform CharTransform;
+    private float MaxHieght;
+    private float MinHieght;
+
+    void Awake()
+    {
+        CharTransform = this.GetComponent<Transform>();
+        MaxHieght = CharTransform.localScale.y;
+        MinHieght = MaxHieght * .95f;
+        CharTransform.DOScale(new Vector3(CharTransform.localScale.x, MinHieght, CharTransform.localScale.z), .8f).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    void Update()
+    {
+        
+    }
+}
