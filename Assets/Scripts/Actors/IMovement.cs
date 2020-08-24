a2e39759ec45a669b31e76aaad02b7e1d144230d
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2020-08-23 21:58:53 -0700

    resolving conflicts

--- Assets/Scripts/Actors/IMovement.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public interface IMovement
-{
-    void TryMove(); //if cant move gets component if there is one in attached to thing in space obj is trying to move to, and call OnCantMove
-    void OnCantMove<T>(T component) where T : Component; //handles logic if obj cant move
-}
