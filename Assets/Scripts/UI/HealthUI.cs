f811cbd3e95b064700b380828d98373ef19e680b
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2020-08-31 14:00:19 -0700

    Health UI working!

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -1,10 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
     public GameObject heart;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
+
+    public static HealthUI Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        SyncHealth();
+    }
 
     public void AddHearts(int num)
     {
@@ -18,6 +36,35 @@ public class HealthUI : MonoBehaviour
 
     public void SyncHealth()
     {
-        
+        if (this.transform.childCount < Player.Instance.playerStats.maxHealth)
+        {
+            AddHearts((Player.Instance.playerStats.maxHealth / 2) - this.transform.childCount);
+        }
+
+        int currPlayerHealth = Player.Instance.playerStats.currentHealth;
+        int maxPlayerHealth = Player.Instance.playerStats.maxHealth;
+
+        if (currPlayerHealth == maxPlayerHealth)
+        {
+            this.transform.GetChild(this.transform.childCount - 1).GetComponent<Image>().sprite = fullHeart;
+            return;
+        }
+
+        if (currPlayerHealth % 2 == 0)
+        {
+            for (int h = (currPlayerHealth/2); h < this.transform.childCount; h++)
+            {
+                this.transform.GetChild(h).GetComponent<Image>().sprite = emptyHeart;
+            }
+        }
+        else
+        {
+            this.transform.GetChild((currPlayerHealth / 2)).GetComponent<Image>().sprite = halfHeart;
+            for (int h = currPlayerHealth / 2 + 1; h < this.transform.childCount; h++)
+            {
+                this.transform.GetChild(h).GetComponent<Image>().sprite = emptyHeart;
+            }
+        }
     }
+        
 }
